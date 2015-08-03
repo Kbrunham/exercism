37247468baf204869f37642b3357bc1ca280fdca
Kalen Brunham <kalen@brunham.net>
2015-08-02 23:22:46 -0400

    Updated with working solution

--- cpp/word-count/word_count.cpp
+++ cpp/word-count/word_count.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <boost/tokenizer.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/trim.hpp>
 
 #include "word_count.h"
 
@@ -10,23 +11,35 @@ std::map<std::string, int> word_count::words(const std::string& str)
 {
 	std::map<std::string, int> counts;
 
-
-	boost::tokenizer<> tok(str);
+    // Using a boost tokenizer with punctuation. This overrides the default
+    // as it removes single quotes as a valid delimieter
+    boost::char_separator<char> sep(" ,!&@$%^&:\n\t.");
+    boost::tokenizer<boost::char_separator<char>> tok(str, sep);
 	for(auto it = tok.begin(); it != tok.end(); ++it)
 	{
-		// Add the word
+		// Convert current string to lower case
 		std::string cur_str = *it;
 		boost::algorithm::to_lower(cur_str);
+        
+        // Remove leading and trailing single quote '
+        boost::algorithm::trim_if(cur_str, [](char c){return c == '\'';});
 
-		auto cit = counts.find(cur_str);
-		if (cit != counts.end())
-		{
-			cit->second++;
-		}
-		else
-		{
-			counts.insert(std::make_pair(cur_str, 1));
-		}
+        // Remove leading and trailing space
+        boost::algorithm::trim(cur_str);
+
+        if (!cur_str.empty())
+        {
+            // Add the word
+            auto cit = counts.find(cur_str);
+            if (cit != counts.end())
+            {
+                cit->second++;
+            }
+            else
+            {
+                counts.insert(std::make_pair(cur_str, 1));
+            }
+        }
 	}
 
 	return counts;
