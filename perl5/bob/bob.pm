a541f4f46ad5d8c3be7c7dfad64e5602868b07fb
Kalen Brunham <kalen@brunham.net>
2015-08-03 20:56:03 -0400

    Initial solution

--- perl5/bob/bob.pm
+++ perl5/bob/bob.pm
@@ -1,24 +1,55 @@
-#
-# This is a SKELETON file and has been provided to enable you to get working on the
-# first exercise more quickly.
-#
 
-use 5.006;
-use strict;
+        package Bob;
+
 use warnings;
+use strict;
+use utf8;
+use Unicode::Normalize;
+use Exporter;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 
-package Bob;
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(hey);
+%EXPORT_TAGS = ( DEFAULT => [qw(&hey)],
+                );
+sub hey($) {
+	my $input = shift(@_);
+	
+	# Remove all accents
+	$input = NFKD($input);
+	print "Word == $input\n";
 
-our $VERSION = '1.000';
+	
+	#Bob answers 'Sure.' if you ask him a question.
+	#He answers 'Whoa, chill out!' if you yell at him.
+	#He says 'Fine. Be that way!' if you address him without actually saying anything.
+	#He answers 'Whatever.' to anything else.
 
-use Exporter 5.57 qw(import);
+	# Shouting is not finding any lower case letter, but finding some upper case letters
+	if (($input !~ m/[a-z]/) && ($input =~ m/[A-Z]/))
+	{
+		return "Whoa, chill out!";
+	}
+	# Empty text
+	elsif ($input =~ m/^\s*$/)
+	{
+		return "Fine. Be that way!";
+	}
+    # Text ending in ?
+	elsif ($input =~ m/\?\s*$/)
+    {
+        return "Sure.";
+    }
+	else
+	{
+		return "Whatever.";
+	}
+	
+	return undef;
 
-our @EXPORT_OK = qw(hey);
-
-sub hey {
-#
-# YOUR CODE GOES HERE
-#
 }
 
 1;
+      
