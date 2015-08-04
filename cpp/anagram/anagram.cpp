ac8e0ef59908925e39b6cb1f93e5b8c7d5205368
Kalen Brunham <kalen@brunham.net>
2015-08-03 20:05:35 -0400

    Added additional comments

--- cpp/anagram/anagram.cpp
+++ cpp/anagram/anagram.cpp
@@ -12,7 +12,7 @@ anagram::finder::finder(const std::string& in_word) :
 {
     boost::algorithm::to_lower(m_original_word);
 
-    // Create the key for the anagrams. This is the lexagraphically sorted and lower case
+    // Create the key for the anagrams. This is the lexicographically sorted and lower case
     // version of the input word.
     boost::algorithm::to_lower(m_sorted_word);
     std::sort(m_sorted_word.begin(), m_sorted_word.end());
@@ -27,21 +27,29 @@ std::vector<std::string> anagram::finder::matches(const std::vector<std::string>
     // iterate through the dictionary vector and return all matches
     for (const auto& word : dict)
     {
-        // It is an anagram if the lexagrapically sorted and lower case
+        // It is an anagram if the lexicographically sorted and lower case
         // version of the dictionary word matches the same operation on the
         // original input word.
-        if (word.length() == m_sorted_word.length())
+
+    	// Step 1: Compare the length
+    	if (word.length() == m_sorted_word.length())
         {
-            std::string sorted_word = word;
+    		// Step 2 : Convert the current word to lower case and compare it to the
+    		// lower case version of the original word. Do this as a word cannot be
+    		// an anagram of itself.
+    		std::string sorted_word = word;
             boost::algorithm::to_lower(sorted_word);
-            
-            // Initial check for self
             if (sorted_word != m_original_word)
             {
-                std::sort(sorted_word.begin(), sorted_word.end());
+
+            	// Step 3: Sort the lower case version of the current word so that it can
+            	// be lexicographically compared to the sorted and lower case version of
+            	// the original word
+            	std::sort(sorted_word.begin(), sorted_word.end());
                 if (sorted_word == m_sorted_word)
                 {
-                    res.emplace_back(word);
+                    // The words are lexicographically equivalent. This is an anagram.
+                	res.emplace_back(word);
                 }
             }
         }
--- cpp/anagram/anagram.h
+++ cpp/anagram/anagram.h
@@ -25,4 +25,3 @@ namespace anagram
         return finder(instr);
     }
 }
-
